ce60db8d27432e77c1af2bf2e3364984cae5e3f4
Camilo Santanilla <camilo.santanilla@pragma.com.co>
2018-12-13 09:27:07 -0500

    V1.0 BETA

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to itwo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Bienvenido a itwo!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { routing } from './app.routing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthenticationService } from './service/authentication.service';
+import { AddUserComponent } from './add-user/add-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import {UserService} from "./service/user.service";
+import { from } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AddUserComponent,
+    EditUserComponent,
+    ListUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [AuthenticationService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
